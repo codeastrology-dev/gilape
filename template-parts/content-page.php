e4e16b28b0dd3d57292c375f0a8ec6e9cd3e0d6f
Moktadir Rahman <codeastrology@gmail.com>
2021-09-06 23:47:39 +0600

    page.php, content-page.php ok

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -26,6 +26,11 @@
 				'after'  => '</div>',
 			)
 		);
+
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 		?>
 	</div><!-- .entry-content -->
 
