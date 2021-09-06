41a9158d222e3160d59555a4c8a2e91944136c24
Moktadir Rahman <codeastrology@gmail.com>
2021-09-06 23:41:19 +0600

    404 page ok

--- 404.php
+++ 404.php
@@ -10,51 +10,25 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="site-content" class="site-main">
+		<div id="primary" class="content-area">
+			<section class="error-404 not-found">
+				<header class="page-header">
+					<h1 class="error-message"><?php esc_html_e( '404', 'gilape' ); ?></h1>
+					<h3 class="page-title"><?php esc_html_e( 'Oops! Looks like this is a dead end. And we know that.', 'gilape' ); ?></h3>
+				</header><!-- .page-header -->
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'gilape' ); ?></h1>
-			</header><!-- .page-header -->
+				<div class="page-content row">
+					<div class="col-md-6 offset-3">
+						<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gilape' ); ?></p>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gilape' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'gilape' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$gilape_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'gilape' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$gilape_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
+						<?php get_search_form(); ?>
+						<a class="btn btn-default"
+                        href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Back to Home', 'gilape' ); ?></a>
+					</div>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+		</div>
 	</main><!-- #main -->
-
 <?php
 get_footer();
