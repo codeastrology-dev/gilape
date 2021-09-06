c363dda43cd5a3f1369eac2bb0aac63540fb812a
Moktadir Rahman <codeastrology@gmail.com>
2021-09-07 00:30:13 +0600

    single.php ok

--- single.php
+++ single.php
@@ -10,31 +10,40 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="site-content" class="site-main container mt-5">
+		<div id="primary" class="content-area">
+			<div class="row">
+				<div class="col-md-8 pr-3 pl-3">
+					<?php
+					while ( have_posts() ) :
+						the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+						get_template_part( 'template-parts/content', 'single' );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+						if ( is_singular( 'post' ) ) {
+							// Post navigation
+							the_post_navigation(
+								array(
+									'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'gilape' ) . '</span> <span class="nav-title">%title</span>',
+									'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'gilape' ) . '</span> <span class="nav-title">%title</span>',
+								)
+							);
+						}
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'gilape' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'gilape' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) :
+							comments_template();
+						endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+					endwhile; // End of the loop.
+					?>
+				</div>
 
-		endwhile; // End of the loop.
-		?>
+				<?php get_sidebar(); ?>
 
+			</div><!-- .row -->
+		</div><!-- #primary -->
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
