eae7b816bcd837558d4876118a415cbc2902142e
Moktadir Rahman <codeastrology@gmail.com>
2021-09-11 16:43:48 +0600

    almost fixed

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -10,51 +10,42 @@
 ?>
 
 <article id="post-<?php the_ID();?>" <?php post_class('mb-5 home-blog'); ?>>
-    <header class="entry-header list">
+    <header class="entry-header mb-4">
         <?php 
             if ( 'post' === get_post_type() ) :
-                if( has_post_thumbnail() ):
-                    gilape_post_thumbnail('gilape-blog-thumbnail');
-                else: ?>
-                    <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/no-thumb.png' ); ?>">
-                    <?php 
+                if( has_post_thumbnail() ): ?>
+					<div class="caption">
+						<?php gilape_post_thumbnail('gilape-blog-thumbnail'); ?>
+					</div>
+					<?php
+				else: ?>
+					<div class="caption">
+						<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/no-thumb.png' ); ?>">
+					</div>
+					<?php 
                 endif;
             endif;
-            gilape_posted_on();
         ?>
     </header><!-- .entry-header -->
-	<div class="entry-content-wrapper">
-		
-		<?php
-		
+	<div class="content-wrapper">
+		<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php
+				gilape_posted_on();
+				gilape_posted_by();
+				?>
+			</div><!-- .entry-meta -->
+		<?php endif;
+		// var_dump( $post);
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title mb-3 mt-4">', '</h1>' );
 		else :
-		?>
-		<h2 class="entry-title mb-3 mt-4">
-			<a href="<?php the_permalink() ?>" rel="bookmark">
-				<?php
-					$the_title = $post->post_title; /* or you can use get_the_title() */
-					$get_length = strlen( $the_title );
-					$the_length = 25;
-					echo substr( $the_title, 0, $the_length );
-					if ( $get_length > $the_length ) echo "...";
-				?>
-			</a>
-		</h2>
-		<?php 
-		endif;?>
+			the_title( '<h2 class="entry-title mb-3 mt-4"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		
-		<div class="blog_post_meta mb-2 mt-2">
-			<?php
-                gilape_posted_by();
-                gilape_posted_on();
-                gilape_get_category();
-			?>
-		</div>
+		endif;?>
 
 		<div class="entry-content clearfix">
-			<?php echo gilape_get_excerpt(50);?>
+			<?php echo gilape_get_excerpt(60);?>
 		</div><!-- .entry-content -->
 
 	</div><!-- .entry-content-wrapper -->
